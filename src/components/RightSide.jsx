ab3b6b61db07d02285430610728083433bcc63ff
Monalisa Biswal <monalisaabiswall@gmail.com>
2025-12-24 20:21:59 +0530

    done with frontend

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -6,6 +6,25 @@ function RightSide() {
       <div className="chat-convo">
 
         <User/>
+        <Bot/>
+        <User/>
+        <Bot/>
+        <User/>
+        <Bot/> 
+        <User/>
+        <Bot/> 
+        <User/>
+        <Bot/> 
+        <User/>
+        <Bot/> 
+        <User/>
+        <Bot/> 
+        <User/>
+        <Bot/> 
+        <User/>
+        <Bot/> 
+        <User/>
+        <Bot/> 
       </div>
       <div className="send-msg">
 
@@ -17,7 +36,24 @@ function RightSide() {
 function User(){
   return(
     <>
-      <h1>jj</h1>
+      <div className="user-box">
+        <div className="user-icon"><i className="fas fa-user-astronaut"></i></div>
+        <div className="user-text">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, deserunt! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, nemo aspernatur accusantium ducimus sint laborum doloribus esse! Id sed eveniet ipsum molestiae, sit, nam mollitia provident temporibus voluptates nemo odio?</p>
+        </div>
+      </div>
+    </>
+  )
+}
+function Bot(){
+  return(
+    <>
+      <div className="user-box">
+        <div className="bot-icon"><img src="lisa-logo.png" width={50} height={50} alt="" /></div>
+        <div className="bot-text">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, deserunt! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, nemo aspernatur accusantium ducimus sint laborum doloribus esse! Id sed eveniet ipsum molestiae, sit, nam mollitia provident temporibus voluptates nemo odio?</p>
+        </div>
+      </div>
     </>
   )
 }
