7f24461667ee0d17e3e4cf57016cebf401c06645
Monalisa Biswal <monalisaabiswall@gmail.com>
2025-12-31 19:31:25 +0530

    fixed the key leaked issue

--- src/gpt.js
+++ src/gpt.js
@@ -1,7 +1,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// Directly use key here
-const genAI = new GoogleGenerativeAI("AIzaSyCYgEy45QTeF0BBtcpmmTBS8xsCNBCle9g");
+const genAI = new GoogleGenerativeAI(
+  import.meta.env.VITE_GEMINI_API_KEY
+);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-2.5-flash",
@@ -9,7 +10,19 @@ const model = genAI.getGenerativeModel({
 
 export async function askGPT(message) {
   const result = await model.generateContent(
-    `You are LisaBot. Reply in simple words. you are a friendly chatbot. when user says hi do introduce yourself with your name that is Lisa\nUser: ${message}`
+    `You are LisaBot.
+You are friendly, chill, and helpful.
+Use simple words and a positive tone.
+
+When the user says hi,
+reply:
+"Hey! I’m Lisa 👋 Your friendly chatbot. What can I help you with?"
+
+Keep replies short and easy to understand.
+
+
+User: ${message}`
   );
+
   return result.response.text();
 }
